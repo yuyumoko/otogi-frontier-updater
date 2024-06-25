0d99bb8b4dc7924500fb1864a74e773c1afad7f7
erinilis <389897773@qq.com>
2024-06-25 14:25:58 +0800

    init

--- config.py
+++ config.py
@@ -17,8 +17,11 @@ if not update_config.has_option(section, "http_proxy"):
 
 http_proxy = update_config.get(section, "http_proxy", fallback="")
 
+update_server_path = Path("update_server.ini")
+if not update_server_path.exists():
+    raise FileNotFoundError(f"you need update_server.ini file")
 
-update_server_config = SimpleConfig(Path("update_server.ini"))
+update_server_config = SimpleConfig(update_server_path)
 resource_repo = update_server_config.get("github", "repo")
 resource_branch = update_server_config.get("github", "branch")
 resource_token = update_server_config.get("github", "token", fallback="")
